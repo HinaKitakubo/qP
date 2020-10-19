2282cdc8c0973f59b1c0e268d502d0d1ee4d1590
HinaKitakubo <40866972+HinaKitakubo@users.noreply.github.com>
2020-10-19 19:59:41 +0900

    Update cmax.hpp

--- cmax.hpp
+++ cmax.hpp
@@ -1,9 +1,10 @@
+// 2分法で求めた x の値から P が最大値（＝cmax）を求める
+
 double cmax_dist(double a, double x, double e)
 {
-      double s = 0.5 * a * a * x;
-      double t = pow(e,5.0);
-      double u = pow(s, 5.0);
+      double t = pow(e, 5.0);
+      double S = 0.5 * a * a * x;
+      double U = pow(S, 5.0);
 
-      return a*a * (1.0 - x) * pow(s, 7.0/2.0) * pow(t+u, -13./20.);
+      return a*a * (1.0 - x) * pow(S, 7.0/2.0) * pow(t+U, -13./20.);
 }
-~ 
